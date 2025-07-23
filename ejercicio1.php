9ae22352339418c8bc6470d02e50cf7515888379
Javier Caál <wilsoncaal0001@gmail.com>
2025-07-22 19:33:51 -0600

    Add files via upload

---
+++ ejercicio1.php
@@ -0,0 +1,15 @@
+<?php
+$numero1 = 50;
+$numero2 = 14;
+
+echo "numero 1 es: $numero1 <br>";
+echo "numero 2 es: $numero2 <br>";
+
+if($numero1 >  $numero2){
+    echo "el numero es mayor $numero1 ";
+
+}else{
+     echo "el numero es mayor $numero2 ";
+}
+
+?>
